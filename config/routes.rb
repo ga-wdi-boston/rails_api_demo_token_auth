3ba3116121f459572c7979205342436f575054c7
David Fisher <tibbon@gmail.com>
2014-11-12 12:50:34 -0500

    Limited routes for user to index, show and create

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users, defaults: { format: :json }
+      resources :users, defaults: { format: :json }, only: [:show, :index, :create]
     end
   end
 end
